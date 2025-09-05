409fe96bb6c26269dbda9d5014041404c272d3a9
Salah-Uddin-Mondal <salah.uddin.oliver@gmail.com>
2025-09-05 22:38:04 +0600

    Sidebar.css

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -60,23 +60,23 @@ const Sidebar = () => {
       <div className='subscribed-list'>
         <h3>Subscribed</h3>
         <div classname='side-link'>
-          <img src={jack} alt='' />
+          <img src={jack} alt='jack' />
           <p>Owen Jones</p>
         </div>
         <div classname='side-link'>
-          <img src={simon} alt='' />
+          <img src={simon} alt='simon' />
           <p>Merriam Music</p>
         </div>
         <div classname='side-link'>
-          <img src={tom} alt='' />
+          <img src={tom} alt='tom' />
           <p>Gamers Nexus</p>
         </div>
         <div classname='side-link'>
-          <img src={megan} alt='' />
+          <img src={megan} alt='megan' />
           <p>Hardware Unboxed</p>
         </div>
         <div classname='side-link'>
-          <img src={cameron} alt='' />
+          <img src={cameron} alt='cameron' />
           <p>Traversy Media</p>
         </div>
       </div>
