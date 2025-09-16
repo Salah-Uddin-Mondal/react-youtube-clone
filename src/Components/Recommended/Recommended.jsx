c397b2d037fe2d125842445134a4b02e3985887c
Salah-Uddin-Mondal <salah.uddin.oliver@gmail.com>
2025-09-16 23:02:45 +0600

    update

---
+++ src/Components/Recommended/Recommended.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Recommended.css';
+import thumbnail1 from '../../assets/thumbnails/thumbnail1.png';
+import thumbnail2 from '../../assets/thumbnails/thumbnail2.png';
+import thumbnail3 from '../../assets/thumbnails/thumbnail3.png';
+import thumbnail4 from '../../assets/thumbnails/thumbnail4.png';
+import thumbnail5 from '../../assets/thumbnails/thumbnail5.png';
+import thumbnail6 from '../../assets/thumbnails/thumbnail6.png';
+import thumbnail7 from '../../assets/thumbnails/thumbnail7.png';
+import thumbnail8 from '../../assets/thumbnails/thumbnail8.png';
+
+const Recommended = () => {
+  return <div>Recommended</div>;
+};
+
+export default Recommended;
